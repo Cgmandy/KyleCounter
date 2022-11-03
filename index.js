3200f7f41f766bf9e4402aa8afd1b2a0ccae0f40
Andy Tsai <cgmandy@gmail.com>
2022-11-02 18:24:53 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  function showPage() {
+    document.getElementById("wholeThingID").style.display = "block";
+    closeForm();
+  }
