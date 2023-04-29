feb4897bf7a878d4a4ad4d239c65ea2a0905a577
atsai <cgmandy@gmail.com>
2023-04-29 08:15:11 -0700

    Added to view full list

---
+++ subpages/crimesList.js
@@ -0,0 +1,53 @@
+var phrases = [
+    "Predicted what he was going to say",
+    "Roasted you for no reason",
+    "Said something that triggered you",
+    "I'm too sober",
+    "My friend (obviously a female)",
+    "We'll see",
+    "We talk later tonight",
+    "I'm broke",
+    "Ripped ass",
+    "Pedo Laugh",
+    "Wait",
+    "I didn't know, did you know",
+    "Fumbled a girl",
+    "Mentioned something Tesla related",
+    "I can't believe"
+  ];
+
+function getAllKyleReasons() {
+    fetch('http://104.172.46.211:5000/kyleGetAllReasons')
+      .then(response => response.json())
+      .then(data => loadHTMLTable(data['data']));
+}
+
+function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody');
+  
+    if (data.length === 0) {
+      table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+      return;
+    }
+  
+    let tableHtml = "";
+  
+    data.forEach(function ({ reason_id, context, date_added, victim }) {
+      tableHtml += "<tr>";
+      tableHtml += `<td>${phrases[reason_id - 1]}</td>`;
+      tableHtml += `<td>${context}</td>`;
+      tableHtml += `<td>${new Date(date_added).toLocaleString()}</td>`;
+      tableHtml += `<td>${victim}</td>`;
+      tableHtml += "</tr>";
+    });
+  
+    table.innerHTML = tableHtml;
+}
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    getAllKyleReasons();
+    
+  
+});
